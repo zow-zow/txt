2e7148281c8415b8afce0997f761fee2c650d9ae
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-08-15 22:35:34 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,13 +1,18 @@
+import os
 import requests
 
 url = "http://freegat.us.kg/"
-
 response = requests.get(url)
 print(response.text)
+
 if response.status_code == 200:
     content = response.text
-    # 将内容写入到txt文件
-    with open("output22.txt", "w", encoding="utf-8") as file:
+    # Get the path to the repository
+    repo_path = os.getenv('GITHUB_WORKSPACE')
+    # Create the full path to the file
+    file_path = os.path.join(repo_path, "output22.txt")
+    # Write the content to the file
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(content)
         print("内容已写入到output.txt文件中")
 else:
