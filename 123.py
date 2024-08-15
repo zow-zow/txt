392e9584ad9047d148a209564d4590a44eb4f5e9
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-08-15 22:27:05 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,22 +1,14 @@
-from bs4 import BeautifulSoup
 import requests
-import re
-import concurrent.futures
 
-# 设置请求头
-headers = {
-    'Host': 'freegat.us.kg',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 XiaoBai1/10.4.5312.1827 (XBCEF)',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6',
-    'Cookie': 'PHPSESSID=65s16sjaus4iag4amk00onk3d3',
-    'Proxy-Connection': 'keep-alive'
-}
-response = requests.get('http://freegat.us.kg/', headers=headers)
-print(response.text)
+url = "http://freegat.us.kg/"
 
-with open('output.txt', 'w', encoding='utf-8') as file:
-    file.write(response.text)
+response = requests.get(url)
+
+if response.status_code == 200:
+    content = response.text
+    # 将内容写入到txt文件
+    with open("output22.txt", "w", encoding="utf-8") as file:
+        file.write(content)
+        print("内容已写入到output.txt文件中")
+else:
+    print("Failed to retrieve content from the URL")
