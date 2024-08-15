68addc4c505c93a18eb5823f085ab574059ef028
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-08-15 22:50:44 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,19 +1,22 @@
-import os
+from bs4 import BeautifulSoup
 import requests
+import re
+import concurrent.futures
 
-url = "http://freegat.us.kg/"
-response = requests.get(url)
+# 设置请求头
+headers = {
+    'Host': 'freegat.us.kg',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 XiaoBai1/10.4.5312.1827 (XBCEF)',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6',
+    'Cookie': 'PHPSESSID=65s16sjaus4iag4amk00onk3d3',
+    'Proxy-Connection': 'keep-alive'
+}
+response = requests.get('http://freegat.us.kg/', headers=headers)
 print(response.text)
 
-if response.status_code == 200:
-    content = response.text
-    # Get the path to the repository
-    repo_path = os.getenv('GITHUB_WORKSPACE')
-    # Create the full path to the file
-    file_path = os.path.join(repo_path, "output22.txt")
-    # Write the content to the file
-    with open(file_path, "w", encoding="utf-8") as file:
-        file.write(content)
-        print("内容已写入到output.txt文件中")
-else:
-    print("Failed to retrieve content from the URL")
+with open('output1111.txt', 'w', encoding='utf-8') as file:
+    file.write(response.text)
