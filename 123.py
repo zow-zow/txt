1d8ec2864689582b8f35327da30090261eac8eeb
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-08-17 11:43:32 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -5,7 +5,7 @@ ffmpeg_command = [
     'ffmpeg',  
     '-re',  
     '-stream_loop', '-1',  
-    '-i', 'http://zowzow.yundown.cf/cscs.php?id=6',  # 替换为你的输入流URL  
+    '-i', 'http://zowzow.yundown.cf/cscs.php?id=1',  # 替换为你的输入流URL  
     '-bsf:a', 'aac_adtstoasc',  
     '-vcodec', 'copy',  
     '-acodec', 'copy',  
